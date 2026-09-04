d4431d86b73ba40d20337291950603b0bc838926
DrBruhman <95565216+DrBruhman@users.noreply.github.com>
2026-09-04 23:58:46 +0500

    just figuring stuff

--- ClientProject/ClientSource/PluginClient.cs
+++ ClientProject/ClientSource/PluginClient.cs
@@ -1,12 +1,43 @@
 ﻿using Barotrauma;
+using Barotrauma.Networking;
 using Barotrauma.Plugins;
 
-namespace ExampleMod;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace QuickInventory;
 
 public partial class Plugin
 {
+    Harmony? harmony;
+    ISettingsService settingsService;
+    ISetting lootKeybind;
     public partial void InitProjectSpecific()
     {
+        harmony = new Harmony("drbruhman.quickinventory");
 
+        harmony.PatchAll();
+
+        settingsService.RegisterSetting(lootKeybind);
+    }
+    public void Dispose()
+    {
+        harmony = null;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CharacterInventory), "Update")]
+    public static void PatchInventoryUpdate(CharacterInventory __instance)
+    {
+        if (Character.Controlled?.Inventory == null ||
+            CharacterHealth.OpenHealthWindow != null ||
+            GameMain.Client.ChatBox.InputBox.Selected) { return; }
+
+        if (PlayerInput.KeyDown(InputType.ShowInteractionLabels))
+        {
+
+        }
     }
 }
--- ServerProject/ServerSource/PluginServer.cs
+++ ServerProject/ServerSource/PluginServer.cs
@@ -1,7 +1,7 @@
 ﻿using Barotrauma;
 using Barotrauma.Plugins;
 
-namespace ExampleMod;
+namespace QuickInventory;
 
 public partial class Plugin
 {
